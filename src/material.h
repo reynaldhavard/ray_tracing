add6ab856d2e753d0e5863e816a20c92f2a2a17e
Reynald Havard <reynald.havard@gmail.com>
2023-11-02 12:42:51 +0000

    base material abstract class

---
+++ src/material.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "color.h"
+#include "ray.h"
+#include "rtweekend.h"
+
+class hit_record;
+
+class material
+{
+public:
+    virtual ~material() = default;
+
+    virtual bool scatter(
+        const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered) const = 0;
+};
